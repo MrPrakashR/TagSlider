86454466ff44ec6476b5127ee7faccd8de6144fc
Prakash Rautela <prakash.rautela@watchyourhealth.com>
2023-07-09 01:01:14 +0530

    Initial Commit

---
+++ TagSlider/TagSlider.h
@@ -0,0 +1,18 @@
+//
+//  TagSlider.h
+//  TagSlider
+//
+//  Created by WYH IOS  on 09/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TagSlider.
+FOUNDATION_EXPORT double TagSliderVersionNumber;
+
+//! Project version string for TagSlider.
+FOUNDATION_EXPORT const unsigned char TagSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagSlider/PublicHeader.h>
+
+
