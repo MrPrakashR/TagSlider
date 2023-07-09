8ad3c7cb7b509c7a45d4531eb5f327f3255eed21
Prakash Rautela <prakash.rautela@watchyourhealth.com>
2023-07-09 19:31:48 +0530

    Initial project setup

---
+++ TagSliderExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TagSliderExample
+//
+//  Created by WYH IOS  on 09/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
